d06c38a026af6a5f57fa474c551e2776e110f666
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-11 14:23:56 -0400

    General bqs

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,18 @@
 const std = @import("std");
 
-pub fn binaryQuickSort(items: []u32, bit: u5) void {
+pub fn binaryQuickSort(comptime T: type, items: []T, context: anytype, at: fn (@TypeOf(context), T, usize) ?bool) void {
+    return bqs_internal(T, items, context, at, 0);
+}
+
+fn bqs_internal(comptime T: type, items: []T, context: anytype, at: fn (@TypeOf(context), T, usize) ?bool, index: usize) void {
     if (items.len <= 1) return;
-    const mask = @shlExact(@intCast(u32, 1), 31 - bit);
     var left: usize = 0;
     var right = items.len - 1;
+    var unfinished: usize = 0;
     while (left <= right) {
-        if (items[left] & mask == 0) {
+        const side = at(context, items[left], index);
+        if (side != null) unfinished += 1;
+        if (side orelse true) {
             left += 1;
         } else {
             std.mem.swap(u32, &items[right], &items[left]);
@@ -14,17 +20,21 @@ pub fn binaryQuickSort(items: []u32, bit: u5) void {
             right -= 1;
         }
     }
-    if (bit < 31) {
-        binaryQuickSort(items[0..left], bit + 1);
-        binaryQuickSort(items[left..], bit + 1);
+    if (unfinished > 1) {
+        bqs_internal(T, items[0..left], context, at, index + 1);
+        bqs_internal(T, items[left..], context, at, index + 1);
     }
 }
 
 const testing = std.testing;
 const asc_u32 = std.sort.asc(u32);
+fn at_asc_u32(context: void, x: u32, i: usize) ?bool {
+    if (i > 31) return null;
+    const mask = @shlExact(@intCast(u32, 1), 31 - @intCast(u5, i));
+    return x & mask == 0;
+}
 test "bqs u32" {
     var items = [_]u32{ 4, 3, 2, 7, 2, 9, 2, 5 };
-    binaryQuickSort(&items, 0);
-    // std.sort.sort(u32, &items, {}, asc_u32);
+    binaryQuickSort(u32, &items, {}, at_asc_u32);
     testing.expect(std.sort.isSorted(u32, &items, {}, asc_u32));
 }
