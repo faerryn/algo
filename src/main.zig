fba681052057b867e9b11426417e3cb529d58eef
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-11 14:12:23 -0400

    binary quick sort is implemented for u32

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,30 @@
 const std = @import("std");
-const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn binaryQuickSort(items: []u32, bit: u5) void {
+    if (items.len <= 1) return;
+    const mask = @shlExact(@intCast(u32, 1), 31 - bit);
+    var left: usize = 0;
+    var right = items.len - 1;
+    while (left <= right) {
+        if (items[left] & mask == 0) {
+            left += 1;
+        } else {
+            std.mem.swap(u32, &items[right], &items[left]);
+            if (right == 0) break;
+            right -= 1;
+        }
+    }
+    if (bit < 31) {
+        binaryQuickSort(items[0..left], bit + 1);
+        binaryQuickSort(items[left..], bit + 1);
+    }
 }
 
-test "basic add functionality" {
-    testing.expect(add(3, 7) == 10);
+const testing = std.testing;
+const asc_u32 = std.sort.asc(u32);
+test "bqs u32" {
+    var items = [_]u32{ 4, 3, 2, 7, 2, 9, 2, 5 };
+    binaryQuickSort(&items, 0);
+    // std.sort.sort(u32, &items, {}, asc_u32);
+    testing.expect(std.sort.isSorted(u32, &items, {}, asc_u32));
 }
